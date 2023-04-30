911b8d9255383dde982b07d273808e6ad7b8ad1a
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-04-30 08:53:57 +0300

    First deployment

---
+++ assets/data.js
@@ -0,0 +1,36 @@
+const items = [
+	{
+		id: 'recmg2a1ctaEJNZhu',
+		name: 'utopia sofa',
+		image: {
+			url: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c29mYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60',
+		},
+		price: 39.95,
+	},
+	{
+		id: 'recvKMNR3YFw0bEt3',
+		name: 'entertainment center',
+		image: {
+			url: 'https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c29mYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60',
+		},
+		price: 29.98,
+	},
+	{
+		id: 'recxaXFy5IW539sgM',
+		name: 'albany sectional',
+		image: {
+			url: 'https://images.unsplash.com/photo-1512212621149-107ffe572d2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60',
+		},
+		price: 10.99,
+	},
+	{
+		id: 'recyqtRglGNGtO4Q5',
+		name: 'leather sofa',
+		image: {
+			url: 'https://images.unsplash.com/photo-1484101403633-562f891dc89a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c29mYXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
+		},
+		price: 9.99,
+	},
+];
+
+module.exports = items;
